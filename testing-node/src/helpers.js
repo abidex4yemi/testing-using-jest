85f5ba8fffffa407b88a36f9e69fbb9888acbe28
abidex4yemi <abidex4yemi@gmail.com>
2019-07-25 18:48:01 +0100

    Test makeUser function

--- testing-node/src/helpers.js
+++ testing-node/src/helpers.js
@@ -2,6 +2,22 @@ function sum(...args) {
 	return args.reduce((total, number) => total + number);
 }
 
+function makeUser({ name, age }) {
+	if (typeof name !== 'string') {
+		throw new Error('String required for name');
+	}
+
+	if (typeof age !== 'number' || isNaN(age)) {
+		throw new Error('Number required for age');
+	}
+
+	return {
+		fullName: name,
+		age
+	};
+}
+
 module.exports = {
-	sum
+	sum,
+	makeUser
 };
--- testing-node/src/helpers.spec.js
+++ testing-node/src/helpers.spec.js
@@ -10,4 +10,19 @@ describe('helpers module', () => {
 			expect(helpers.sum(2, 3, 5)).toEqual(10);
 		});
 	});
+
+	describe('Test suit for makeUser()', () => {
+		it('Should return an object', () => {
+			expect(helpers.makeUser({ name: 'Yemi', age: 20 })).toEqual({ fullName: 'Yemi', age: 20 });
+		});
+
+		it('Should throw an error if name is not a string', () => {
+			expect(() => helpers.makeUser({ name: 5, age: 1 })).toThrow();
+		});
+
+		it('Should throw an error if age is not a number', () => {
+			expect(() => helpers.makeUser({ name: 'Yemi', age: '' })).toThrow();
+			expect(() => helpers.makeUser({ name: 'Yemi', age: NaN })).toThrow();
+		});
+	});
 });
